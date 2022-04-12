65ce9079dd01aecb653fef091030306c82497321
joshua0983 <joshuajateno@gmail.com>
2022-04-12 21:14:02 +0300

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,17 +3,18 @@
 /**
  * jack_bauer -prints every minute of the day
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 
 void jack_bauer(void)
+
 {
 int a;
 int b;
 
 for (a = 0; a <= 23; a++)
 {
-for (b = 0; b <= 59, b++)
+for (b = 0; b <= 59; b++)
 {
 _putchar (a / 10 + '0');
 _putchar (a % 10 + '0');
@@ -21,6 +22,10 @@ _putchar (':');
 _putchar (b / 10 + '0');
 _putchar (b % 10 + '0');
 _putchar ('\n');
+
 }
+
+
 }
+
 }
