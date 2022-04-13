c35b2fecf340eaaa9956b1a48779f48ede4f3abc
joshua0983 <joshuajateno@gmail.com>
2022-04-13 18:55:13 +0300

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * largest_number - returns the largest of 3 numberrs
- * @a: firsrt interger
- * @b: second interger
- * @c: thrid interger 
+ * largest_number - returns the largest of 3 numbers
+ * @a: firsrt integer
+ * @b: second integer
+ * @c: third integer 
  * Return: largest number
  */
 
